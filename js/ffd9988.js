0679a10371d4d53985cc9a1d9d0689aee06ac9f7
Zaid <itsunknown122@gmail.com>
2023-09-13 07:50:15 +0000

    Delete js directory

--- js/ffd9988.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[38],{446:function(n,t,s){"use strict";s.r(t);var u=s(5),e=Object(u.a)({},(function(){return(0,this._self._c)("path",{attrs:{d:"M8.29498 16.885L12.875 12.295L8.29498 7.705L9.70498 6.295L15.705 12.295L9.70498 18.295L8.29498 16.885Z"}})}),[],!1,null,null,null);t.default=e.exports}}]);
